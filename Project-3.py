e09d267522875c39d3e5076a6b90af2bc60bb423
Berserkered <sohampatel2003@gmail.com>
2022-01-26 22:42:16 -0500

    adding committed

--- Project-3.py
+++ Project-3.py
@@ -67,83 +67,150 @@ elif project_identifier == 'P3B':
 # ---------------------------------------------------------------------------------
 
 
+def checkContainer():
+    table.rotate_table_angle(45)
+
+    container = []
+
+    checkColorone = table.photoelectric_sensor(0.2)
+    checkColortwo = table.inductive_sensor(0.2)
+    print(checkColorone)
+    print(checkColortwo)
+
+    if (checkColorone[0] > 4 and checkColortwo[0] > 4):
+        container.append("metal")
+    elif (checkColorone[0] < 1 and checkColortwo[0] < 1):
+        container.append("plastic")
+    else:
+        container.append("paper")
+
+    checkClean = table.load_cell_sensor(0.2)
+    container.append(checkClean)
+
+    if ((container[0] == "plastic" and checkClean[0] > 9.5) or (container[0] == "metal" and checkClean[0] > 15.5) or (container[0] == "paper" and checkClean[0] > 10.5)):
+        container.append("dirty")
+    else:
+        container.append("clean")
+
+    if (container[0] == "metal"):
+        container.append("bin1")
+    elif ((container[0] == "plastic" or container[0] == "paper") and container[1] == "dirty"):
+        container.append("bin4")
+    elif (container[0] == "plastic"):
+        container.append("bin3")
+    else:
+        container.append("bin2")
+
+    table.rotate_table_angle(45)
+
+    return container
+
+
+def genRand():
+    randNum = random.randint(1, 6)
+    table.dispense_container(randNum)
+
+
 def dispenseContainer():
     if table.proximity_sensor_short():
+        attributes = checkContainer()
+    else:
+        genRand()
+        time.sleep(1)
+        attributes = checkContainer()
+    return attributes
 
-        rotate_table_angle(45)
 
-        container = []
+def pickContainer():
+    time.sleep(1)
+    arm.move_arm(0.66, 0, 0.25)
+    time.sleep(1)
+    arm.control_gripper(45)
+    time.sleep(1)
 
-        checkColorone = table.photoelectric_sensor(0.2)
-        checkColortwo = table.inductive_sensor(0.2)
-        if (checkColorone[0] > 4 and checkColorone[0] > 4):
-            container.append("metal")
-        elif (checkColorone[0] < 1 and checkColorone[0] < 1):
-            container.append("plastic")
+
+def firstPick():
+    time.sleep(1)
+    arm.move_arm(-0.11, -0.565, 0.6)
+    time.sleep(2)
+    arm.control_gripper(-35)
+    time.sleep(1)
+    arm.rotate_shoulder(-40)
+    time.sleep(1)
+    arm.home()
+    time.sleep(1)
+
+
+def secondPick():
+    time.sleep(1)
+    arm.move_arm(-0.2, -0.565, 0.6)
+    time.sleep(2)
+    arm.control_gripper(-35)
+    time.sleep(1)
+    arm.rotate_shoulder(-40)
+    time.sleep(1)
+    arm.home()
+    time.sleep(1)
+
+
+def thirdPick():
+    time.sleep(1)
+    arm.move_arm(0.07, -0.565, 0.6)
+    time.sleep(2)
+    arm.control_gripper(-35)
+    time.sleep(1)
+    arm.rotate_shoulder(-40)
+    time.sleep(1)
+    arm.home()
+    time.sleep(1)
+
+
+loadedContainer = []
+weight = 0
+binNum = True
+loop = True
+
+bot.rotate(98.12)
+while (loop == True):
+    currContainer = dispenseContainer()
+    time.sleep(1)
+    print(currContainer)
+    loadedContainer.append(currContainer)
+    weight += loadedContainer[0][1][0]
+    currBin = loadedContainer[0][3]
+    pickContainer()
+    firstPick()
+    time.sleep(1)
+    currContainer = dispenseContainer()
+    time.sleep(1)
+    print(currContainer)
+    loadedContainer.append(currContainer)
+    weight += loadedContainer[1][1][0]
+    currBintwo = loadedContainer[1][3]
+    if ((currBintwo == currBin) and (weight < 90)):
+        pickContainer()
+        secondPick()
+        time.sleep(1)
+        currContainer = dispenseContainer()
+        time.sleep(1)
+        print(currContainer)
+        loadedContainer.append(currContainer)
+        weight += loadedContainer[2][1][0]
+        currBinthree = loadedContainer[2][3]
+        if ((currBinthree == currBin)and (weight <90)):
+            pickContainer()
+            thirdPick()
         else:
-            container.append("paper")
+            table.rotate_table_angle(270)
+            loop = False
+    else:
+        table.rotate_table_angle(270)
+            loop = False
+    loop = False
 
-        checkClean = table.load_cell_sensor(0.2)
-        if ((container[0] == "plastic" and checkClean[0] > 9.5) or (container[0] == "metal" and checkClean[0] > 15.5) or (container[0] == "paper" and checkClean[0] > 10.5)):
-            container.append("dirty")
-        else:
-            container.append("clean")
 
-        if (container[0] == "metal"):
-            container.append("bin1")
-        elif ((container[0] == "plastic" or container[0] == "paper") and container[1] == "dirty"):
-            container.append("bin4")
-        elif (container[0] == "plastic"):
-            container.append("bin3")
-        else:
-            container.append("bin2")
 
-        rotate_table_angle(45)
 
-        return container
-
-def loadContainer():
-    bot.rotate(98.12)
-    def pickContainer():
-        time.sleep(1)
-        arm.move_arm(0.66, 0, 0.25)
-        time.sleep(1)
-        arm.control_gripper(45)
-        time.sleep(1)
-
-    def firstPick():
-        time.sleep(1)
-        arm.move_arm(-0.11, -0.57, 0.6)
-        time.sleep(2)
-        arm.control_gripper(-35)
-        time.sleep(1)
-        arm.rotate_shoulder(-40)
-        time.sleep(1)
-        arm.home()
-        time.sleep(1)
-
-    def secondPick():
-        time.sleep(1)
-        arm.move_arm(-0.2, -0.57, 0.6)
-        time.sleep(2)
-        arm.control_gripper(-35)
-        time.sleep(1)
-        arm.rotate_shoulder(-40)
-        time.sleep(1)
-        arm.home()
-        time.sleep(1)   
-       
-    def thirdPick():
-        time.sleep(1)
-        arm.move_arm(0.07, -0.57, 0.6)
-        time.sleep(2)
-        arm.control_gripper(-35)
-        time.sleep(1)
-        arm.rotate_shoulder(-40)
-        time.sleep(1)
-        arm.home()
-        time.sleep(1)
-    
 
 # ---------------------------------------------------------------------------------
 # STUDENT CODE ENDS
